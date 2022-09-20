1a3bb110282306cd3051a2e3057892801891e0e9
Raquel Revelles <raquel@Raquels-MacBook-Pro.local>
2022-09-20 09:10:49 +0100

    initial commit

---
+++ src/pet_shop.py
@@ -0,0 +1,83 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop,amount):
+   amount == 10
+   pet_shop["admin"]["total_cash"] += amount
+   pet_shop["admin"]["total_cash"] - amount
+
+#    pet_shop["admin"]["total_cash"] -= amount
+#    pet_shop["admin"]["total_cash"] = 990
+ 
+#  if total_cash += 
+
+# def add_or_remove_cash(pet_shop,amount):
+#     pet_shop["admin"]["total_cash"] = 990
+
+
+# def add_or_remove_cash(pet_shop,amount): 
+#    amount == 10
+#    add_cash = pet_shop["admin"]["total_cash"] + amount
+#    remove_cash = pet_shop["admin"]["total_cash"] - amount
+
+# def my_function(): 
+#     if something == True:
+#        return "it"s true"
+#     else: 
+#         return "it's false" 
+
+# def add_or_remove_cash(pet_shop, amount): #for both exercises , add or remove cash , a function that works for multiple things
+#     add_cash(pet_shop, 100)
+
+# def get_total_cash(pet_shop):
+#     return pet_shop["admin"]["total_cash"]
+
+# def get_pets_sold(pet_shop):
+#     if pet_shop["admin"]["pets_sold"] == 0:
+#         return pet_shop["admin"]["pets_sold"]
+#     else:
+#         pet_shop["admin"]["pets_sold"] = 2
+
+# def get_pets_sold(pet_shop):
+
+#     if pet_shop["admin"]["pets_sold"] == 0:
+#         return pet_shop["admin"]["pets_sold"]
+#     else:
+#         increase_pets_sold = pet_shop["admin"]["pets_sold"] + 2
+#         return increase_pets_sold
+
+# def get_pets_sold(pet_shop):
+# pet_shop["admin"]["pets_sold"] = 2
+
+# def get_stock_count(pet_shop):
+#     count = 0
+#     for pet in pet_shop["pets"]:
+#         count += 1
+#     return count   
+
+# def get_pets_by_breed(pet_shop,pet_breed):
+#     found_breed_total = None
+#     for pet_breed in pet_shop["pets"]:
+#         if pet_breed["name"] == "British Shorthair":
+#             found_breed_total = pet_breed
+#             print(len(found_breed_total))
+#     return found_breed_total
+
+    
+ 
+
+# def find_pet_by_name(pet_shop):
+#     found_pet = None
+#     for pet in pet_shop:
+#         if pet["Arthur"] == pet_name:
+#             found_pet = pet
+#     return found_pet
+
+# def get_customer_cash(customers):
+#     return customers[0]["cash"]
+
+       
